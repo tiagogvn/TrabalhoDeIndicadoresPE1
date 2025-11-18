e8bff20039b28e03cd2565797295e807b1887cf3
Rikhael <fe_ceacero@id.uff.br>
2025-11-18 15:41:42 -0300

    Subindo script com informações premilinares da contrução da base

---
+++ scriptConstruçãodaBase.R
@@ -0,0 +1,38 @@
+# Este é o script onde construiremos a base de dados sobre mortalidade para os muni-
+#cípios do Rio de Janeiro. Feita a análise exploratória da nossa base de dados, se-
+#lecionamos 12 indicadores que consideramos de alguma forma estratégicos para a aná-
+#lise da mortalidade no Estado. 
+# Aqui, iremos identificar as informações que devemos utilizar como variáveis nos
+#indicadores que criamos, renomeá-las para fácil visualização e padronização, e
+#calculá-las para cada município.
+
+# O objetivo final é uma base onde cada linha é um Município do Rio de Janeiro, e
+# cada coluna um dos indicadores criados, preenchidos com seus valores calculados
+#para cada município.
+
+library(geobr) #pacote para extrair código, nome e dados de cada município
+library(dplyr) #pacote para manipulação de dados
+library(sf)
+
+# Baixar lista de municípios do RJ
+mun_rj <- read_municipality(code_muni = "RJ", year = 2023) |>
+  st_drop_geometry() |>
+  select(code_muni, name_muni)
+
+# Criando base com 12 colunas preenchidas apenas com o nome dos indicadores, e
+#todos os municípios do Rio de Janeiro nas linhas
+base_indicadores <- mun_rj |>
+  mutate(
+    TxMFAG23 = NA_real_,
+    TxMNDC23 = NA_real_,
+    TxMPADR23 = NA_real_,
+    TxMMVP23 = NA_real_,
+    TxMPMDPNN23 = NA_real_,
+    TxMCEPP23 = NA_real_,
+    TxFNM23 = NA_real_,
+    TxMAT23 = NA_real_,
+    TxMLAP23 = NA_real_,
+    TxMDAF23 = NA_real_,
+    PMDCv23 = NA_real_,
+    PMCMD23 = NA_real_
+  )
